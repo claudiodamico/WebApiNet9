b71e7571fff3d52d08ed6a91706e07c47cbbeada
«claudiodamico» <cdamico18@gmail.com>
2024-12-09 15:04:11 -0300

    Eliminado submódulo, añadido como directorio regular

---
+++ CrudNet9/Controllers/VideoGameController.cs
@@ -0,0 +1,76 @@
+﻿using CrudNet9.Data;
+using CrudNet9.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController(VideoGameDbContext context) : ControllerBase
+    {
+        private readonly VideoGameDbContext _context = context;
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<VideoGame>>> GetVideoGames()
+        {
+            return Ok(await _context.VideoGames.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<VideoGame>>> GetVideoGameById(int id)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            if (game is null)
+                return NotFound();
+
+            return Ok(game);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VideoGame>> AddVideoGame(VideoGame newGame)
+        {
+            if (newGame is null)
+
+                return BadRequest();
+
+            _context.VideoGames.Add(newGame);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVideoGame(int id, VideoGame updatesGame)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            if (game is null)
+                return NotFound();
+
+            game.Title = updatesGame.Title;
+            game.Platform = updatesGame.Platform;
+            game.Developer = updatesGame.Developer;
+            game.Publisher = updatesGame.Publisher;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVideoGame(int id)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            if (game is null)
+                return NotFound();
+
+            _context.VideoGames.Remove(game);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}   
+
---
+++ CrudNet9/Data/VideoGameDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CrudNet9.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet9.Data
+{
+    public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
+    {
+        public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+    }
+}
